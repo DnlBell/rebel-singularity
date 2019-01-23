63279893624e0a9ac701103beeae09338b02d7bb
dnlbell <dnl.bell@gmail.com>
2019-01-22 17:00:19 -0800

    cleaning up App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import GameView from './view/GameView.js';
 
-
-const GameFrame = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 10px;
-`;
-
 class App extends Component {
   render() {
     return (
